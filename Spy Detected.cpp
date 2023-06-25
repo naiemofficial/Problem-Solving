e530f2db7f40b5889edef606519abb36f65b659c
Abdullah Al Naiem <mail@naiem.info>
2023-06-26 04:33:31 +0600

    June 26, 2023 => [CF] [D26] - [B1]

---
+++ Spy Detected.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int t; cin >> t; while(t--){
+        int n; cin >> n;
+        vector<int> a(n, 0);
+        for(int i = 0; i < n; i++) cin >> a[i];
+        for(int i = 0; i < n; i++){
+            if((i > 0 && i < n && a[i] != a[i+1] && a[i+1] == a[i-1]) ||
+               (i == 0 && a[i] != a[i+1] && a[i+1] == a[i+2])        || 
+               (i == n-1 && a[i] != a[i-1] && a[i-1] == a[i-2])){
+                cout << i+1 << endl;
+                break;
+            }
+        }
+    }
+}

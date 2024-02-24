37859d7152de7891f94ce9f8519ab583f4cd4aa8
Abdullah Al Naiem <mail@naiem.info>
2024-02-25 05:22:34 +0600

    January 25, 2024 => [S2] [CF][D44] - [B1]

--- Plus-Minus Split - 1919B.cpp
+++ Plus-Minus Split - 1919B.cpp
@@ -5,11 +5,11 @@ int main(){
     int t; cin >> t; while(t--){
         int n; cin >> n;
         string s; cin >> s;
-        
+
         int sum = 0;
         for(int i = 0; i < n; i++){
             sum += (s[i] == '+') ? 1 : -1;
         }
         cout << abs(sum) << endl;
     } 
-}
+} 
---
+++ Wallet Exchange - 1919A.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t; cin >> t; while(t--){
+        int a, b; cin >> a >> b;
+        cout << (((a+b) % 2 == 0) ? "Bob" : "Alice") << endl;
+    }
+}
